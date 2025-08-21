de984865753112e8b78fec60352eb93dd37d6f35
oiottj <oiottj@gmail.com>
2025-08-22 08:54:13 +0900

    modify requestMatchers resource

--- src/main/java/fog/booking_service/controller/HeathCheckController.java
+++ src/main/java/fog/booking_service/controller/HeathCheckController.java
@@ -1,5 +1,6 @@
 package fog.booking_service.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class HeathCheckController {
     @GetMapping("/health")
     public ResponseEntity<Map<String, Object>> health() {
+        log.info("health check");
         Map<String, Object> res = new HashMap<>();
         res.put("status", "UP");
         res.put("service", "booking-service");
